4a600e9cee7f36db015ebe650ac35d3004bc180e
Julien Richard <45456851+jurichar@users.noreply.github.com>
2024-03-17 12:30:54 +0100

    Add go.mod and vendor files for rss_feed_aggregator project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module rss_feed_aggregator
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	fmt.Printf("port: %v", portString)
+}
